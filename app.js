5af1f3c211f7595273fd7821c66a9c4169fb6b66
Lucia Sotomayor <cia811114@gmail/com>
2025-04-18 10:17:46 -0700

    exercises 4.1-4.2: blogs project

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const blogRouter = require('./controllers/blogs')
+
+//Conexión a la base de datos MongoDB
+mongoose.set('strictQuery', false)
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogRouter)
+
+module.exports = app
