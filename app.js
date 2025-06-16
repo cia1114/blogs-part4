127e8eb97e4739b6e709e61ab321d77928bc8335
Lucia Sotomayor <cia811114@gmail/com>
2025-06-16 16:16:48 -0700

    4.15 - 4.17 blogs: adding users

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 const blogRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
+const userRouter = require('./controllers/users')
 
 //Conexión a la base de datos MongoDB
 mongoose.set('strictQuery', false)
@@ -20,7 +21,7 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogRouter)
-
+app.use('/api/users', userRouter)
 app.use(middleware.errorHandler)
 
 module.exports = app
--- utils/middleware.js
+++ utils/middleware.js
@@ -1,10 +1,10 @@
 const errorHandler = (err, req, res, next) => {
   if (err.name === 'ValidationError') {
     return res.status(400).json({ message: err.message })
-  }
-  if (err.name === 'CastError') {
+  } else if (err.name === 'CastError') {
     return res.status(400).send({ error: 'malformatted id' })
-  }
+  } else if (err.name === 'MongoServerError' && err.message.includes('E11000 duplicate key error'))
+    return res.status(400).json({ error: 'expected `username` to be unique' })
   next(err)
 }
 
