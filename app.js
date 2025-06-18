f64ce500c34a8976d5cb1f7d5afed546255c6304
Lucia Sotomayor <cia811114@gmail/com>
2025-06-18 10:13:30 -0700

    4.18 - 4.19 token authentication

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const logger = require('./utils/logger')
 const blogRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
 const userRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 //Conexión a la base de datos MongoDB
 mongoose.set('strictQuery', false)
@@ -20,6 +21,7 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', userRouter)
 app.use(middleware.errorHandler)
